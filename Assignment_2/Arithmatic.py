a2b2c4ea29cba05c97ebb22ba657f8aaf1ab3f8b
VaishaliMJ <jorwekar.vaishali@gmail.com>
2025-05-16 17:20:28 +0530

    First commit
    Arithmetic.py module

---
+++ Assignment_2/Arithmatic.py
@@ -0,0 +1,33 @@
+"""-------------------------------------------------------------------------------------------
+                    Assignment2 : Arthimatic Module
+                    (Student name - Vaishali Jorwekar)
+                     Python by Marvellous Infosystems
+---------------------------------------------------------------------------------------------
+Module : This is a module which contains basic arithmatic functions like :
+       --> Add(num1,num2)
+       --> Sub(num1,num2)
+       --> Mult(num1,num2)
+       --> Div(num1,num2)
+----------------------------------------------------------------------------------------------
+
+"""
+#Addition function with two parameters
+def Add(num1,num2):
+    return num1 + num2
+
+
+#Subtraction function with two parameters
+def Sub(num1,num2):
+    return num1 - num2
+
+#Multiplication function with two parameters
+def Mult(num1,num2):
+    return num1 * num2
+
+#Division function with two parameters
+def Div(num1,num2):
+    return num1 / num2
+
+
+
+
